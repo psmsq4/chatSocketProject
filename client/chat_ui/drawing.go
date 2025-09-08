12630fe3c785620fcb78ed99f32964eb797999d4
psmsq4 <shshin378@gmail.com>
2025-09-08 21:04:11 +0900

    .

--- client/chat_ui/drawing.go
+++ client/chat_ui/drawing.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 	"time"
 
 	gc "github.com/gbin/goncurses"
@@ -15,8 +16,15 @@ const (
 	TNewline = 1
 )
 
+var isChatLogCond = true // 채팅 로그 조건 변수
+var mutex sync.Mutex
+var chatLogCond sync.Cond
+var msgInputCond sync.Cond
+
 func DrawFrame(chatRoomID int16, messageListener chan string, chatLogFileDescriptor *os.File) {
 	var msg string
+	chatLogCond.L = &mutex
+	msgInputCond.L = &mutex
 
 	chatLog := make([]byte, 1024)
 
@@ -43,28 +51,40 @@ func DrawFrame(chatRoomID int16, messageListener chan string, chatLogFileDescrip
 		log.Fatal(err)
 	}
 
+	stdscr.ScrollOk(true)
+
 	max_y, max_x := stdscr.MaxYX()
 	height_chatLogArea := max_y / 10 * 7
-	height_wirteMsgArea := max_y / 10 * 3
+	height_writeMsgArea := max_y / 10 * 3
 
 	startRow := 0
-	endRow := height_chatLogArea - 1
 
 	chatLogArea, _ := gc.NewWindow(height_chatLogArea, max_x-20, 0, 20)
-
 	defer chatLogArea.Delete()
-	writeMsgArea, _ := gc.NewWindow(height_wirteMsgArea+1, max_x, height_chatLogArea, 0)
+	writeMsgArea, _ := gc.NewWindow(height_writeMsgArea+1, max_x, height_chatLogArea, 0)
 	defer writeMsgArea.Delete()
+
+	chatLogArea.ScrollOk(true)
+	writeMsgArea.ScrollOk(true)
 
 	go DrawWriteMsgArea(chatRoomID, writeMsgArea, messageListener)
 
 	for {
+		chatLogCond.L.Lock()
+		for !isChatLogCond {
+			chatLogCond.Wait()
+		}
 		chatLogArea.Erase()
 		chatLogArea.MovePrint(1, 0, string(chatLog))
 		chatLogArea.Border(gc.ACS_BULLET, gc.ACS_BULLET, gc.ACS_HLINE, gc.ACS_HLINE, gc.ACS_HLINE, gc.ACS_HLINE, gc.ACS_HLINE, gc.ACS_HLINE)
 
 		chatLogArea.Refresh()
 		chatLogArea.Keypad(false)
+
+		isChatLogCond = false
+		msgInputCond.Broadcast()
+		chatLogCond.L.Unlock()
+
 		msg = <-messageListener
 
 		// nextLine += 2
@@ -75,25 +95,32 @@ func DrawFrame(chatRoomID int16, messageListener chan string, chatLogFileDescrip
 		} else if strings.Compare(msg, "/c up") == 0 {
 			chatLogFileDescriptor.ReadAt(chatLog, 0)
 			startRow--
-			endRow--
-			chatLog = []byte(ExtractValidVolumnChatlog(startRow, endRow, string(chatLog)))
+			chatLog = []byte(ExtractValidVolumnChatlog(&startRow, height_chatLogArea, string(chatLog)))
 		} else if strings.Compare(msg, "/c down") == 0 {
 			chatLogFileDescriptor.ReadAt(chatLog, 0)
 			startRow++
-			endRow++
-			chatLog = []byte(ExtractValidVolumnChatlog(startRow, endRow, string(chatLog)))
+			chatLog = []byte(ExtractValidVolumnChatlog(&startRow, height_chatLogArea, string(chatLog)))
 		} else {
 			chatLogFileDescriptor.WriteString(msg)
 			chatLogFileDescriptor.ReadAt(chatLog, 0)
-			chatLog = []byte(ExtractValidVolumnChatlog(startRow, endRow, string(chatLog)))
+
+			startRow = -1
+
+			chatLog = []byte(ExtractValidVolumnChatlog(&startRow, height_chatLogArea, string(chatLog)))
 		}
 	}
 }
 
-func ExtractValidVolumnChatlog(startRow, endRow int, originChatlog string) string {
+func ExtractValidVolumnChatlog(startRow *int, chatLogAreaHeight int, originChatlog string) string {
+	/*
+		서버로부터 메세지를 받으면 game.go에서는 아래와 같이 메세지를 후처리한다.
+		broadcastMessageFormatString := fmt.Sprintf(" %s  |  %s\n [%d]: %s\n\n", sender, time_chat, broadcastMessage.MessageSequence, message)
+		_MessageListener <- broadcastMessageFormatString
+	*/
 	targetStrings := make([]string, 0)
 
 	startIndex := 0
+	chatLogAreaHeight = chatLogAreaHeight - 2 // 상하 테두리 제외
 
 	for currentIndex, char := range originChatlog {
 		if char == '\n' {
@@ -102,14 +129,28 @@ func ExtractValidVolumnChatlog(startRow, endRow int, originChatlog string) strin
 		}
 	}
 
-	if startRow < 0 {
-		startRow = 0
+	if *startRow < 0 { // /c up 시 시작 인덱스가 0보다 작아지는 경우 또는 새 메세지가 도착했을 때
+		if len(targetStrings)-chatLogAreaHeight < 0 {
+			*startRow = 0
+		} else {
+			/* 전체 메세지 개수가 100개라 가정하자.
+			 * 그리고 실제 채팅창 높이가 1이라 가정하자.
+			 * 그러면 화면에 출력되어야 할 메세지는 [99, 100) == [99, 99] == 99번째 인덱스에 있는 메세지이다.
+			 * 따라서 시작 인덱스를 99로 설정해야 한다.
+			 * 이는 높이가 n이어도 성립한다.
+			 */
+			*startRow = len(targetStrings) - chatLogAreaHeight
+		}
+	} else if *startRow > (len(targetStrings) - 3) {
+		*startRow = len(targetStrings) - 3
 	}
+
+	endRow := *startRow + chatLogAreaHeight
 	if endRow > len(targetStrings) {
 		endRow = len(targetStrings)
 	}
 
-	convertedString := strings.Join(targetStrings[startRow:endRow], "\n")
+	convertedString := strings.Join(targetStrings[*startRow:endRow], "\n") // go도 python처럼 끝인덱스는 포함하지 않는다.
 	targetStrings = nil
 	return convertedString
 }
@@ -118,15 +159,49 @@ func DrawWriteMsgArea(chatRoomID int16, writeMsgArea *gc.Window, messageListener
 	time.Sleep(10 * time.Millisecond)
 	// var buffer string
 
+	writeMsgArea.Keypad(true)
 	for {
+		msgInputCond.L.Lock()
+		for isChatLogCond {
+			msgInputCond.Wait()
+		}
 		writeMsgArea.Erase()
 		writeMsgArea.Border(gc.ACS_BULLET, gc.ACS_BULLET, gc.ACS_HLINE, gc.ACS_HLINE, gc.ACS_HLINE, gc.ACS_HLINE, gc.ACS_HLINE, gc.ACS_HLINE)
-		writeMsgArea.MovePrint(1, 1, "New Message > ")
-		writeMsgArea.Refresh()
-		message, err := writeMsgArea.GetString(512)
-		if err != nil {
-			panic(err)
+		// message, err := writeMsgArea.GetString(512)
+		prefix := "New Message > "
+		message := prefix
+		prefix_length := len(prefix)
+		for {
+			writeMsgArea.MovePrint(1, 1, message)
+			writeMsgArea.Refresh()
+
+			char := writeMsgArea.GetChar()
+			if char == gc.KEY_RETURN {
+				break
+			}
+
+			if char == gc.KEY_UP {
+				message = "New Message > /c up"
+				break
+			} else if char == gc.KEY_DOWN {
+				message = "New Message > /c down"
+				break
+			} else if char == gc.KEY_BACKSPACE {
+				if len(message) > prefix_length {
+					runes := []rune(message)
+					runes = runes[:len(runes)-1]
+					message = string(runes)
+				}
+			} else {
+				message += string(char)
+			}
 		}
+
+		message = message[prefix_length:]
+
+		// if err != nil {
+		// 	panic(err)
+		// }
 		if strings.Compare(message, "quit") == 0 {
 			messageListener <- "quit"
 			break
@@ -138,5 +213,8 @@ func DrawWriteMsgArea(chatRoomID int16, writeMsgArea *gc.Window, messageListener
 			// 입력받은 메세지를 서버로 전송해야함.
 			send_request.SendTransferMessage(chatRoomID, message)
 		}
+		isChatLogCond = true
+		chatLogCond.Broadcast()
+		msgInputCond.L.Unlock()
 	}
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module src
 go 1.22.5
 
 require github.com/bzick/tokenizer v1.4.0
+
+require github.com/gbin/goncurses v0.0.0-20240517145248-be6a464272ae // indirect
